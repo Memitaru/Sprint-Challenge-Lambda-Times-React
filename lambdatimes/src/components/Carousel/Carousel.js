852a5e4a39a17bd26ac05ee341f94b62cdbb0082
Memitaru <amandatwah@gmail.com>
2019-03-24 20:30:42 -0700

    Stretch goal carousel complete

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,42 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      images: [],
+      indexer: 0
     }
   }
   componentDidMount(){
-
+    this.setState({
+      images: carouselData,
+    })
   }
 
   leftClick = () => {
-
+    this.setState({
+      indexer: this.state.indexer < this.state.images.length - 1 ? this.state.indexer + 1 : 0
+    })
   }
 
   rightClick = () => {
-
+    this.setState({
+      indexer: this.state.indexer > 0 ? this.state.indexer - 1 : this.state.images.length - 1
+    });
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return(
+      <img src={this.state.images[this.state.indexer]}
+      style={{display: 'block'}}
+      alt=""
+      />
+    )
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        <this.selectedImage />
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
